08b5f9235896d15ab0458dcfdb63091bb9b0f797
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:36:11 -0400

    kitabo/ensi/ukubona/donorcase/test/assets/js/script.js

--- kitabo/ensi/ukubona/donorcase/test/assets/js/script.js
+++ kitabo/ensi/ukubona/donorcase/test/assets/js/script.js
@@ -203,3 +203,4 @@ window.addEventListener('load', function() {
 document.getElementById("calculate-risk-button").addEventListener("click", function() {
     throttledCalculateRisk();
 });
+# flick 20250409213611-KQIl
