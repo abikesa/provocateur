debbbe2e3d77ba17c1a10cc5b37a4c83a88c9635
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:36:03 -0400

    kitabo/ensi/bash/jbp-https.sh

--- kitabo/ensi/bash/jbp-https.sh
+++ kitabo/ensi/bash/jbp-https.sh
@@ -3,4 +3,4 @@ rm -rf _build
 pip install -r requirements.txt && jb build .
 cp -r part1/app/* _build/html/part1/app
 cd ~/documents/rhythm
-local/kitabo/ensi/scripts/jbp-https.sh
+local/kitabo/ensi/scripts/jbp-https.sh# flick 20250409213603-jNab
